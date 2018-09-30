6d669b8dd96cbacfb1049f695ceb00dacc6294ed
SHollatz <ztallohs@gmail.com>
2018-09-29 23:35:54 -0700

    complete app without article summary

---
+++ server.js
@@ -0,0 +1,152 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+// Initialize Express
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("./public"));
+
+// Connect to the Mongo DB
+// mongoose.connect("mongodb://localhost/news-scraper", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+//Routes
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.get("/scrape", function (req, res) {
+  request("http://www.dw.com/", function (error, response, html) {
+    if (error) {
+      print("error", error);
+    }
+
+    var $ = cheerio.load(html);
+    var result = {};
+
+    $("div .news").each(function (i, element) {
+
+      var link = $(element).children().attr("href");
+      result.link = "www.dw.com" + link;
+      //console.log("$(element).children().text().trim()", $(element).children().text().trim());
+      var title = $(element).children().text().trim();
+      //var summary = $(element).children().html();
+      //console.log("summary", summary);
+      if (title.match(/\n/g)) {
+        result.title = title.split("\n")[0];
+      } else {
+        result.title = title;
+      }
+      // console.log("result.title", result.title);
+      //results.push(result);
+
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+    });
+
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.send("Scrape Complete");
+  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function (dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for grabbing a specific Article by id, populate it with it's note
+app.get("/articles/:id", function (req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function (dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+app.post("/articles/:id", function (req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function (dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function (dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+app.put("/articles/:id", function (req, res) {
+  // Create a new note and pass the req.body to the entry
+  console.log("inside put");
+  console.log("req.params.id", req.params.id);
+  //console.log("article",db.Article.findOne({ _id: req.params.id }))
+  db.Article.findOneAndUpdate({ _id: req.params.id }, {$unset: {note:""}})
+    .then(function (article) {
+      console.log(article);
+      console.log("id" + article.note._id);
+      // db.Article.findOneAndUpdate({_id:article._id}, {$unset: {note:article.note._id}});
+      db.Note.findByIdAndDelete(article.note._id);
+      return article 
+    })
+    .then(function (dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      console.log(err);
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function () {
+  console.log("App running on port " + PORT + "!");
+});
